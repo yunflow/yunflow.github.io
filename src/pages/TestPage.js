fa802202087626aeadf9a56b569fecf7b0570ec0
Jamie Wang <dhsuicos@yahoo.com>
2023-07-10 22:36:31 -0300

    Update React

---
+++ src/pages/TestPage.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+
+function TestPage() {
+    return (
+        <>
+            <Typography>
+                Hello, this is test1
+            </Typography>
+        </>
+    );
+}
+
+export default TestPage;
